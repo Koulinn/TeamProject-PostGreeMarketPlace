85c74b69461058f7620698bc993a7435f370f67b
Raia Kisseljova <84378854+Raissa-Kisseljova@users.noreply.github.com>
2021-08-24 20:44:43 +0100

    Added PUT and DELETE for Reviews

--- src/services/reviews/handlers.js
+++ src/services/reviews/handlers.js
@@ -24,7 +24,6 @@ export const postReview = async (req, res, next) => {
     console.log(req.params)
     const review = await db.query(`INSERT INTO public.reviews(
             review, rate, product_id)
-            
             VALUES ('${req.body.review}','${req.body.rate}','${req.params.product_id}') RETURNING * ; `);
     res.status(201).send(review.rows[0]);
   } catch (error) {
@@ -35,6 +34,11 @@ export const postReview = async (req, res, next) => {
 
 export const editReview = async (req, res, next) => {
   try {
+    const review =
+      await db.query(`UPDATE public.reviews SET review ='${req.body.review}',
+			 rate = '${req.body.rate}'
+       WHERE review_id=${req.params.review_id} RETURNING *;`);
+    res.send(review.rows[0]);
   } catch (error) {
     console.log(error);
     next(error);
@@ -43,6 +47,10 @@ export const editReview = async (req, res, next) => {
 
 export const deleteReview = async (req, res, next) => {
   try {
+    const review = await db.query(
+      `DELETE FROM public.reviews WHERE review_id=${req.params.review_id};`
+    );
+    res.status(204).send();
   } catch (error) {
     console.log(error);
     next(error);
