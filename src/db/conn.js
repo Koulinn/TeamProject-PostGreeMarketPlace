b9cf3f687097fa18029e9ab92e9b6d5249abde6b
Koulinn <drdverzola@gmail.com>
2021-08-25 08:10:19 +0100

    commented conn

--- src/db/conn.js
+++ src/db/conn.js
@@ -2,21 +2,21 @@ import pg from "pg";
 
 const { Pool } = pg;
 
-const db = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  password: '123',
-  port: 3010,
-});
 // const db = new Pool({
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-//   connectionString:
-//     process.env.NODE_ENV !== "development"
-//       ? process.env.DATABASE_URL
-//       : process.env.DATABASE_URL_DEV,
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'postgres',
+//   password: '123',
+//   port: 3010,
 // });
+const db = new Pool({
+  // ssl: {
+  //   rejectUnauthorized: false,
+  // },
+  connectionString:
+    process.env.NODE_ENV !== "development"
+      ? process.env.DATABASE_URL
+      : process.env.DATABASE_URL_DEV,
+});
 
 export default db;
